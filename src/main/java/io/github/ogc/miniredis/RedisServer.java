3e2700616ba486cd4bae5e8312aa03b1ebed363a
0G-C <218736830+0G-C@users.noreply.github.com>
2026-07-03 17:02:50 +0800

    feat: bootstrap redis server with logback

---
+++ src/main/java/io/github/ogc/miniredis/RedisServer.java
@@ -0,0 +1,10 @@
+package io.github.ogc.miniredis;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class RedisServer {
+    public static void main(String[] args) {
+        log.info("mini-redis booting");
+    }
+}
